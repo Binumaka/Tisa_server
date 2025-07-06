27998728df8fe9af99811d9d53bb945b257694b7
Binu Maka <binumaka9@gmail.com>
2025-07-07 03:02:58 +0545

    updated app.js and others

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const connectDB = require("./config/db");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
 const authRoute = require("./routes/authRoutes");
+const ornamentRoute = require("./routes/ornamentRoute");
+const packageRoute = require("./routes/packageRoutes");
+const wishlistRoutes = require("./routes/wishlistRoutes");
+const shoppingRoutes = require("./routes/shoppingCartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const rentRoutes = require("./routes/rentRoutes");
+const rentOrnamentRoutes = require("./routes/rentOrnamentRoutes");
+const rentOrderRoutes = require("./routes/rentorderCompleteRoutes");
 
 connectDB();
 
@@ -17,6 +25,16 @@ app.use(
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoute);
+app.use("/api/ornament", ornamentRoute);
+app.use("/api/package", packageRoute);
+app.use("/api/wishlist", wishlistRoutes);
+app.use("/api/cart", shoppingRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/rent", rentRoutes);
+app.use("/api/rentOrnament", rentOrnamentRoutes);
+app.use("/api/rentorder", rentOrderRoutes);
+app.use('/ornaments_image', express.static("ornaments_image"));
+app.use('/uploads', express.static("uploads"));
 
 const port = 3000;
 app.listen(port, () => {
--- security/Auth.js
+++ security/Auth.js
@@ -11,13 +11,13 @@ const authenticateToken = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    const user = await User.findById(decoded._id); // Make sure `_id` is included in the JWT payload
+    const user = await User.findById(decoded._id); 
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    req.user = user; // ✅ Assign user to request
+    req.user = user; 
     next();
   } catch (error) {
     console.error('Token verification failed:', error);
